918c40beba96becd3a89e3240e4ffc45e8e04463
Venturesum1 <soumyasisdas8@gmail.com>
2025-02-28 03:31:39 +0530

    first commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,22 @@
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { Component } from 'lucide-react'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+      {/* <ClerkProvider>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+    <Component />
+  </ClerkProvider> */}
+    </div>
+  )
+}
+
+export default Home
